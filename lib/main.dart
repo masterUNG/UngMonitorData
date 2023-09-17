9423a612d7d13e1da4b829034da4c6218ea49053
masterUNG <masterUng@abc.com>
2023-09-17 11:52:01 +0700

    finish 17sep

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ungmonitordata/states/authen.dart';
 
-
-
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-
-  
-
-  runApp(MyApp());
+  await Firebase.initializeApp().then((value) => runApp(MyApp()));
 }
-
-
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
--- lib/utility/app_service.dart
+++ lib/utility/app_service.dart
@@ -1,12 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 class AppService {
-
-
-  void processSentOTP({required String phoneNumber}) {
+  Future<void> processSentOTP({required String phoneNumber}) async {
     String userPhoneNumber = phoneNumber;
 
+    userPhoneNumber = '+66 ${userPhoneNumber.substring(1)}';
+
     print('userPhoneNumber ---> $userPhoneNumber');
+
+    await FirebaseAuth.instance.verifyPhoneNumber(
+      phoneNumber: userPhoneNumber,
+      timeout: const Duration(seconds: 60),
+      verificationCompleted: (phoneAuthCredential) {},
+      verificationFailed: (error) {},
+      codeSent: (verificationId, forceResendingToken) {
+        String verId = verificationId;
+        print('verId ====> $verId');
+      },
+      codeAutoRetrievalTimeout: (verificationId) {},
+    );
   }
-
-
-  
 }
